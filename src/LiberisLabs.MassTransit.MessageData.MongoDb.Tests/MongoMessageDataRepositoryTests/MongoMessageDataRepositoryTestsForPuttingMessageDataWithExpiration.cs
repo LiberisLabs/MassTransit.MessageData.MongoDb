c0a8574c73db16200029f2395c52ea98ad06687b
Blundell89 <leeblundell16@gmail.com>
2015-11-18 08:54:01 +0000

    Added tests

---
+++ src/LiberisLabs.MassTransit.MessageData.MongoDb.Tests/MongoMessageDataRepositoryTests/MongoMessageDataRepositoryTestsForPuttingMessageDataWithExpiration.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using Moq;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace LiberisLabs.MassTransit.MessageData.MongoDb.Tests.MongoMessageDataRepositoryTests
+{
+    public class MongoMessageDataRepositoryTestsForPuttingMessageDataWithExpiration
+    {
+        private GridFSBucket _bucket;
+        private byte[] _expected;
+        private ObjectId _id;
+        private TimeSpan _expectedTtl;
+
+        [TestFixtureSetUp]
+        public void GivenAMongoMessageDataRepository_WhenPuttingMessageDataWithExpiration()
+        {
+            var db = new MongoClient().GetDatabase("messagedatastoretests");
+            _bucket = new GridFSBucket(db);
+            var fixture = new Fixture();
+            _expected = fixture.Create<byte[]>();
+            var resolver = new Mock<IMongoMessageUriResolver>();
+            resolver.Setup(m => m.Resolve(It.IsAny<ObjectId>())).Returns((ObjectId x) => new Uri("urn:" + x));
+            var sut = new MongoMessageDataRepository(resolver.Object, _bucket);
+            _expectedTtl = TimeSpan.FromHours(1);
+
+            using (var stream = new MemoryStream(_expected))
+            {
+                var uri = sut.Put(stream, _expectedTtl).GetAwaiter().GetResult();
+                _id = new ObjectId(uri.AbsoluteUri.Split(':').Last());
+            }
+        }
+        
+        [Test]
+        public async Task ThenMessageStoredAsExpected()
+        {
+            var result = await _bucket.DownloadAsBytesAsync(_id);
+
+            Assert.That(result, Is.EqualTo(_expected));
+        }
+
+        [Test]
+        public async Task ThenExpirationSetAsExpected()
+        {
+            var cursor = await _bucket.FindAsync(Builders<GridFSFileInfo>.Filter.Eq("_id", _id));
+            var list = await cursor.ToListAsync();
+            var doc = list.Single();
+
+            var expiration = doc.Metadata["expiration"].ToUniversalTime();
+            Assert.That(expiration, Is.LessThan(DateTime.UtcNow.Add(_expectedTtl)));
+            Assert.That(expiration, Is.GreaterThan(DateTime.UtcNow.Add(_expectedTtl.Add(TimeSpan.FromMinutes(-1)))));
+        }
+
+        [TestFixtureTearDown]
+        public void Kill()
+        {
+            _bucket.DropAsync().GetAwaiter().GetResult();
+        }
+    }
+}
