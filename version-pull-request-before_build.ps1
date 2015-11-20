c298a68ef5d5b9623113a146cce5c3dcefd8c250
Blundell89 <leeblundell16@gmail.com>
2015-11-20 11:21:07 +0000

    Updated config

--- version-pull-request-before_build.ps1
+++ version-pull-request-before_build.ps1
@@ -3,5 +3,5 @@ Write-Host "Checking if pull request"
 if ([bool]$env:APPVEYOR_PULL_REQUEST_NUMBER)
 {
    Write-Host "Build is pull request - updating version"
-   Update-AppveyorBuild -Version "0.0.0-pull-request-$($env:APPVEYOR_BUILD_NUMBER)"
+   Update-AppveyorBuild -Version "0.0.0-pull-request-$($env:APPVEYOR_PULL_REQUEST_NUMBER)-$($env:APPVEYOR_BUILD_NUMBER)"
 }
