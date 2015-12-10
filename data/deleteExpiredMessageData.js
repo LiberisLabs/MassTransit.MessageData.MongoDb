5d0ac7386098d89ce32550405c2df5360ad3e959
Blundell89 <leeblundell16@gmail.com>
2015-12-10 16:15:29 +0000

    Removed date variable as not required

--- data/deleteExpiredMessageData.js
+++ data/deleteExpiredMessageData.js
@@ -1,8 +1,7 @@
 (function() {
 	var docs = db.getMongo().getDB("masstransit");
-	var now = new Date().toISOString();
 
-	var cursor = docs.fs.files.find({"metadata.expiration" : {$lte : new Date(now)}});
+	var cursor = docs.fs.files.find({"metadata.expiration" : {$lte : new Date()}});
 	
 	cursor.forEach(function (toDelete) {
 		var id = toDelete._id;
